5f6560edf85bf26966cc395ba6f28df5c5f5ef9e
lucho1503 <lmmc.1503@gmail.com>
2019-09-17 23:09:13 +0000

    1-element_at.py_fixed

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:
-        if i > idx:
+    for i, ent in enumerate(my_list):
+        if idx < 0:
             return None
-        elif idx < 0:
-            return None
-        else:
-            if idx == i:
-                return my_list[i]
+        if idx == i:
+            return ent
+    return None
