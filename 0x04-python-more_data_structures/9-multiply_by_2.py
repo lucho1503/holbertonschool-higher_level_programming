5eecd3d9d93a99e501e8e31a53ca6202887d01ef
lucho1503 <lmmc.1503@gmail.com>
2019-09-20 06:50:00 +0000

    9-multiply_by_2.py_fixed

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-def mul(num):
-    return num * 2
-
 def multiply_by_2(a_dictionary):
     new = {}
     for i in a_dictionary:
-      new[i] = (a_dictionary[i] * 2)
+        new[i] = (a_dictionary[i] * 2)
     return new
