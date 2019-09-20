23718c99a1de9fc365d2aa1f71a84a01351788c7
lucho1503 <lmmc.1503@gmail.com>
2019-09-20 03:53:36 +0000

    2-uniq_add.py

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    suma = 0
+    if my_list is None:
+        return suma
+    else:
+        mylist = list(set(my_list))        
+        for i in mylist:
+            suma = suma + i
+        return suma
