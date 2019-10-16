9f749da69c27e4ea1705e82e6c5adf056961f83a
lucho1503 <lmmc.1503@gmail.com>
2019-10-16 05:07:43 +0000

    fix

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 class BaseGeometry:
     def area(self):
         raise Exception('area() is not implemented')
@@ -8,6 +8,7 @@ class BaseGeometry:
             raise TypeError(name + ' must be an integer')
         if value <= 0:
             raise ValueError(name + ' must be greater than 0')
+
 
 class Rectangle(BaseGeometry):
     def __init__(self, width, height):
