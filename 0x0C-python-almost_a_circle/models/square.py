4864f16de866ac63eb513c077d00f54c9f94533e
lucho1503 <lmmc.1503@gmail.com>
2019-10-24 04:14:37 +0000

    comented code

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -6,8 +6,10 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """inizializate method"""
+    """ class Rectancgle """
+
     def __init__(self, size, x=0, y=0, id=None):
+        """inizializate method"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
