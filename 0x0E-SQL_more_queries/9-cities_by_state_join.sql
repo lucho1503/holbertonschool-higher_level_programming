f245e3d3aaf8c11c814f6c5e49e4db7cbea1867a
lucho1503 <lmmc.1503@gmail.com>
2019-11-29 22:22:50 +0000

    fix query

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- this query lists all cities in the database
-SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON cities.id = states.id;
+SELECT cities.id, cities.name, states.name FROM states INNER JOIN cities ON cities.state_id = states.id;
