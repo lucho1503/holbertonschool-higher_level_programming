51b44036a8828edddf7169fb01eb34c8976b52d2
lucho1503 <lmmc.1503@gmail.com>
2019-11-29 04:09:29 +0000

    2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,6 @@
 -- this instruction create a database and user and give permision by user
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
-CREATE USER IF NOT EXISTS 'user_0d_2@localhost' IDENTIFIED BY 'user_0d_2_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_2@localhost';
+SET PASSWORD FOR user_0d_2@localhost = 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
-FLUSH PRIVILEGES;
+
