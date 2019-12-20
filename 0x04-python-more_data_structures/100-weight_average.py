38e4a464a1d497b0ce56e460c30571b1a621a653
lucho1503 <lmmc.1503@gmail.com>
2019-12-20 18:18:05 +0000

    100-weight_average.py

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    div_0 = 0
+    div_1 = 1
+    milist = []
+    for i, j in my_list:
+        div_0 += i * j
+        milist.append(j)
+    if len(milist) > 0:
+        div_1 = 0
+    for num in milist:
+        div_1 += num
+    return div_0 /div_1
