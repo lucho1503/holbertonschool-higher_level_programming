0f949d51a98c4e07a7a907eac6fe11c33834841e
lucho1503 <lmmc.1503@gmail.com>
2019-12-21 06:43:57 +0000

    16-shows_by_genre.sql

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- this query list all shows and genres linked to taht show
+
+SELECT tv_shows.title, tv_genres.name
+FROM tv_shows
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name
