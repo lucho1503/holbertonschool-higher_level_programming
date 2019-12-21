433c9e1ec2304b71dd5a369ed610f3154e79ed6e
lucho1503 <lmmc.1503@gmail.com>
2019-12-21 06:47:22 +0000

    fix

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,8 +2,8 @@
 
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
-JOIN tv_show_genres
+LEFT OUTER JOIN tv_show_genres
 ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_genres
+LEFT OUTER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 ORDER BY tv_shows.title, tv_genres.name
