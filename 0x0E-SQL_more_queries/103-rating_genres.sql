ac3c9e67e5e47f1b7495e145581b81c27c29138e
lucho1503 <lmmc.1503@gmail.com>
2019-12-22 00:50:49 +0000

    103-rating_genres.sql

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,7 @@
+-- this query lists all genres in the database
+
+SELECT tv_genres.name, Sum(tv_show_ratings.rate) AS rating
+FROM tv_genres INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id INNER JOIN tv_show_ratings
+ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY tv_genres.name ORDER BY rating DESC;
