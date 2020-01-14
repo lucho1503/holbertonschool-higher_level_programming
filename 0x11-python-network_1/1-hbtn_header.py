750e39447d8b3a045cb5b2ccdf38b5e7d559608e
lucho1503 <lmmc.1503@gmail.com>
2020-01-14 16:54:57 +0000

    1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" thus script sends a request to the URL and display the value """
+
+import sys
+import urllib.request
+
+with urllib.request.urlopen(sys.argv[1]) as fi:
+    print(fi.headers['X-Request-Id'])
