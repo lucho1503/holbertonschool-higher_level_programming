74df9c82d77109c22001f207986e957a21ee1ef4
lucho1503 <lmmc.1503@gmail.com>
2020-01-14 18:03:20 +0000

    2-post_email.py

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" this scirpt sends a request and takes a email as argument """
+
+import sys
+import urllib.request
+import urllib
+
+
+if __name__ == "__main__":
+
+    email = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(email)
+    data = data.encode('ascii')
+    url = urllib.request.Request(sys.argv[1], data)
+    with urllib.request.urlopen(url) as fi:
+        print (str(fi.read(), 'utf-8'))
