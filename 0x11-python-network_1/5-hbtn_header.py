56b79a154b2ae57083df07092910fba63181a32c
lucho1503 <lmmc.1503@gmail.com>
2020-01-15 03:06:18 +0000

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" this script takes a URL and sends a request to the URL """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+
+    r = requests.get(sys.argv[1])
+    print(r.headers['X-Request-Id'])
