f5028e019c4c566e2fca15d8cf939ad278ef1f5d
lucho1503 <lmmc.1503@gmail.com>
2020-02-04 21:15:59 +0000

    1-multi_languages.js

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myVarc = 'C is fun';
+const myVarp = 'Python is cool';
+const myVarJ = 'Javascript is amazing';
+console.log(myVarc);
+console.log(myVarp);
+console.log(myVarJ);
