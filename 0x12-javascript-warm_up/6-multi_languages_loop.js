b096567c3737933a06ad17ebe668f0404db2407c
lucho1503 <lmmc.1503@gmail.com>
2020-02-05 04:28:46 +0000

    6-multi_languages_loop.js

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let index = 0;
+for (index = 0; index < myArray.length; index++) {
+  console.log(myArray[index]);
+}
