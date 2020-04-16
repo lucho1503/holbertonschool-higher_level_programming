18860da57135878c577c972493c6b51fef8b7df8
lucho1503 <lmmc.1503@gmail.com>
2020-04-16 18:25:32 +0000

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# this script sends a POST reqeusts with a letter as a parametersimport sys
+
+
+import requests
+if __name__ == "__main__":
+
+    if len(sys.argv) > 1:
+        msj = {'q': sys.argv[1]}
+    else:
+        msj = {'q': ""}
+
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data=msj)
+
+    try:
+        res = r.json()
+        if len(res) > 0:
+            print("[{}] {}".format(res.get('id'), res.get('name')))
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
