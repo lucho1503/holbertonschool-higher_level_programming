f532d6619d5d79c564a6c7ebd2066993fd2d15de
lucho1503 <lmmc.1503@gmail.com>
2020-04-16 18:05:12 +0000

    executable_file

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,9 +8,11 @@ from requests.exceptions import HTTPError
 
 
 if __name__ == "__main__":
+
     try:
-        res = requests.get(sys.argv[1])
-        res.raise_for_status()
-        print(res.text)
+        response = requests.get(sys.argv[1])
+        response.raise_for_status()
+        print(response.text)
+
     except HTTPError as error:
         print('Error code: {}'.format(error.response.status_code))
