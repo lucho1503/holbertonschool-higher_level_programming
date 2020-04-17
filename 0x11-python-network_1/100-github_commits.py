5d71d4124da9b050e3e53bcbcaffeafa67e58216
lucho1503 <lmmc.1503@gmail.com>
2020-04-17 16:03:27 +0000

    100-github_commits.py

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this script print commits for sha commit and author """
+
+
+from requests import *
+import sys
+
+
+if __name__ == "__main__":
+
+    user = sys.argv[1]
+    owner = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(user, owner)
+    res = get(url)
+    res_json = res.json()
+    for i in res_json:
+        sha = i.get('sha')
+        commit = i.get('commit')
+
+        if commit:
+            author = commit.get('author')
+        if author:
+            name = author.get('name')
+            print('{}: {}'.format(sha, name))
