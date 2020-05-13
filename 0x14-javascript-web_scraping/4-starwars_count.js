2ac2abb5127cb4911ec4e5ca0d1e3ade728b559a
lucho1503 <lmmc.1503@gmail.com>
2020-05-13 02:20:14 +0000

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const request = require('request');
+const match = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/people/18/';
+
+let count = 0;
+
+request(match, function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  }
+
+  const res = JSON.parse(body);
+  for (const i in res.results) {
+    if (res.results[i].characters.includes(url)) {
+      count++;
+    }
+  }
+  console.log(count);
+});
