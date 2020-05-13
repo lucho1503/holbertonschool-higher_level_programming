8926cfa90f22f284f51b37b95214a2559b4df044
lucho1503 <lmmc.1503@gmail.com>
2020-05-13 01:56:57 +0000

    3-starwars_title.js

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+
+request(url, function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
